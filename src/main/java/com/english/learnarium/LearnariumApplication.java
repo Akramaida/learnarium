1d4229908e611ef430525ae01279f0be3bccc2d7
Akraman Gorbakov <amir.gorskiy@gmail.com>
2024-02-12 19:48:41 +0300

    Start project

---
+++ src/main/java/com/english/learnarium/LearnariumApplication.java
@@ -0,0 +1,13 @@
+package com.english.learnarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnariumApplication.class, args);
+	}
+
+}
